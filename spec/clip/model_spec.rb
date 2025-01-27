b43e331afc73ff67b9a943844d1fc2fa7be87097
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2025-01-27 20:39:33 +0100

    Add image test

--- spec/clip/model_spec.rb
+++ spec/clip/model_spec.rb
@@ -3,9 +3,22 @@
 RSpec.describe Clip::Model do
   let(:model) { described_class.new }
   describe '#encode_text' do
-    it 'encodes text' do
+    it 'encodes text', aggregate_failures: true do
       text = 'a photo'
-      expect(model.encode_text(text)).to be_a(Array)
+      result = model.encode_text(text)
+      expect(result).to be_a(Array)
+      expect(result.size).to eq(512)
+      expect(result.all? { |x| x.is_a?(Float) }).to be_truthy
+    end
+  end
+
+  describe '#encode_image' do
+    it 'encodes an image' do
+      image = File.open('spec/fixtures/test.jpg')
+      result = model.encode_image(image)
+      expect(result).to be_a(Array)
+      expect(result.size).to eq(512)
+      expect(result.all? { |x| x.is_a?(Float) }).to be_truthy
     end
   end
 end
