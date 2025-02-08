60d98c3acd219f98ebce37c8a05dee9b450c3d58
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2025-02-08 22:02:05 +0100

    Fix model downloader

--- lib/clip.rb
+++ lib/clip.rb
@@ -3,6 +3,7 @@ require_relative "clip/multilingual_model"
 require_relative "clip/tokenizer"
 require_relative "clip/image_preprocessor"
 require "net/http"
+require "uri"
 require "fileutils"
 require "logger"
 
@@ -11,42 +12,51 @@ module Clip
 
   BASE_URL = "https://huggingface.co/khasinski/"
   MODELS = {
-    textual: "clip-ViT-B-32-onnx/resolve/main/textual.onnx?download=true",
-    visual: "clip-ViT-B-32-onnx/resolve/main/visual.onnx?download=true"
+    "textual.onnx" => "clip-ViT-B-32-onnx/resolve/main/textual.onnx?download=true",
+    "visual.onnx" => "clip-ViT-B-32-onnx/resolve/main/visual.onnx?download=true"
   }
 
   MULTILINGUAL_MODELS = {
-    textual: "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/XLM-Roberta-/textual.onnx?download=true",
-    textual_bin: "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/data.bin?download=true",
-    visual: "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/visual.onnx?download=true"
+    "textual.onnx" => "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/textual.onnx?download=true",
+    "data.bin" => "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/data.bin?download=true",
+    "visual.onnx" => "XLM-Roberta-Large-Vit-B-32-onnx/resolve/main/visual.onnx?download=true"
   }
 
   def self.download_models(download_dir, models = MODELS)
     logger ||= Logger.new(STDOUT)
     FileUtils.mkdir_p(download_dir)
 
-    models.each do |type, path|
+    models.each do |filename, path|
       uri = URI.join(BASE_URL, path)
-      logger.info("Downloading #{type} model from #{uri}")
+      logger.info("Downloading #{filename} model from #{uri}")
 
-      while true
-        response = Net::HTTP.get_response(uri)
+      self.download_file(uri.to_s, File.join(download_dir, filename))
+    end
+  end
 
-        if response.is_a?(Net::HTTPRedirection)
-          logger.info("Redirected to #{response['location']}")
-          uri = URI.parse(response['location']) # Update URI to the redirect location
-          next
-        elsif response.is_a?(Net::HTTPSuccess)
-          file_path = File.join(download_dir, "#{type}.onnx")
-          File.open(file_path, 'wb') do |file|
-            file.write(response.body) # Write the body directly for simplicity
+  def self.download_file(url, destination, limit = 10)
+    raise "Too many HTTP redirects" if limit == 0
+
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    http.request(request) do |response|
+      case response
+      when Net::HTTPRedirection
+        new_url = response['location']
+        self.download_file(new_url, destination, limit - 1)
+      when Net::HTTPSuccess
+        File.open(destination, 'wb') do |file|
+          response.read_body do |chunk|
+            file.write(chunk)
           end
-          logger.info("Successfully downloaded #{type} model")
-          break
-        else
-          logger.error("Failed to download #{type} model from #{uri}: #{response.code} #{response.message}")
-          raise "Failed to download #{type} model from #{uri}"
         end
+        puts "Downloaded #{url} to #{destination}"
+      else
+        raise "Failed to download file: #{response.code} #{response.message}"
       end
     end
   end
